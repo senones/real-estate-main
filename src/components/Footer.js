aebff073eda21dadb0885c168327a445bf8fb7ae
senones <59428039+senones@users.noreply.github.com>
2023-04-18 12:56:57 +0200

    first

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='bg-black py-8 text-center text-white'>
       <div className='container mx-auto'>
-        Copyright &copy; 2022. All rights reserved.
+        Copyright &copy; Senon 2023. All rights reserved.
       </div>
     </footer>
   );
